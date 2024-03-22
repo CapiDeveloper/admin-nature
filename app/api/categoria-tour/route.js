4ecb56c1560be7a2d41687f218c26936844ea8bd
Bryan <morocho.12chacha@gmail.com>
2024-03-22 15:40:43 -0500

    Configuracion package

--- app/api/categoria-tour/route.js
+++ app/api/categoria-tour/route.js
@@ -2,8 +2,6 @@ import { NextResponse, NextRequest } from "next/server";
 import prisma from "../../../lib/prisma";
 
 export async function GET(request) {
-
     const categoria_tours = await prisma.CategoriaTour.findMany();
-
-    return NextResponse.json( categoria_tours );
+    return NextResponse.json(categoria_tours);
 }
